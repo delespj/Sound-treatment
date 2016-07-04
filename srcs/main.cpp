f6e201ae9d833e7b00df0f3f76efe7bf3826c549
Tulkas <joseph.delespinay@epitech.eu>
2016-07-04 15:48:28 +0200

    circle done

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,24 +1,10 @@
+#include <stdlib.h>
 #include "visualizer.hh"
-
-void	init_height(int height[180])
-{
-  for (int i = 0; i < 180; i++)
-    {
-      if (i % 2 == 0)
-	height[i] = 50;
-      else
-	height[i] = 20;
-    }
-}
 
 int     main()
 {
   Visualizer	vis(800, 600);
-  int	height[180];
 
-  init_height(height);
-  vis.drawCircle(height);
-  vis.display();
   vis.loop();
   return (0);
 }
